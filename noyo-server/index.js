f0a8f4ea0405e00eb6ca9230134ba6b2ee1202cd
Noyo Lama <noyolopchan0102@gmail.com>
2025-11-07 21:10:14 +0545

    Added noyo-server folder

---
+++ noyo-server/index.js
@@ -0,0 +1,45 @@
+// index.js ecommerce-backend
+ 
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const connectDB = require('./config/db');
+const cookieParser = require('cookie-parser');
+const initializeApp = require('./scripts/init');
+
+// Load env vars
+dotenv.config({
+  debug: true
+});
+
+// Connect to database
+connectDB().then(async () => {
+  await initializeApp()
+})
+
+const app = express();
+
+// Enable CORS
+app.use(cors({
+  origin: ['https://noyo.vercel.app', 'http://localhost:5173', 'https://noyo.onrender.com'],
+  credentials: true,
+}));
+// Body parser middleware
+app.use(express.json());
+// Cookie parser middleware
+app.use(cookieParser());
+
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
+const productRoutes = require('./routes/productRoutes');
+const orderRoutes = require('./routes/orderRoutes');
+const userRoutes = require('./routes/userRoutes');
+app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
+app.use('/api/orders', orderRoutes);
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`Server running on port ${PORT}`));
+
+module.exports = app
